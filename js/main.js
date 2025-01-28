37af80a09bc67071ce13fe437ceb47dbfbffcad6
xenosM <105599352+xenosM@users.noreply.github.com>
2025-01-28 14:47:19 +0545

    drumstick animations and basic project design complete

--- js/main.js
+++ js/main.js
@@ -1,47 +1,65 @@
-import * as sound from './sounds.js'
-import { playAudio } from './utils.js'
+import * as sound from "./sounds.js";
+import { playAudio, animateDrumstick } from "./utils.js";
 
 //*VARIABLE DECLARATION
 //*DOM elements
-const volumeInput = document.querySelector('#volume-range')
-const volumeIndicator = document.querySelector('#volume-indicator')
+const volumeInput = document.querySelector("#volume-range");
+const volumeIndicator = document.querySelector("#volume-indicator");
+//*Drumsticks
+const drumstickHihat = document.querySelector(".drumstick-hihat");
+const drumstickCrash = document.querySelector(".drumstick-crash");
+const drumstickSnare = document.querySelector(".drumstick-snare");
+const drumstickHighTom = document.querySelector(".drumstick-high-tom");
+const drumstickLowTom = document.querySelector(".drumstick-low-tom");
+const drumstickRide = document.querySelector(".drumstick-ride");
+const drumstickFloorTom = document.querySelector(".drumstick-floor-tom");
 
 //*Variables
-let volume=1;
+let volume = 1;
 
 //*Play sound on key press
-document.addEventListener('keydown',e =>{
-    const key = e.key.toLowerCase()
-    e.stopPropagation()
-    switch(key){
-        case 'a':
-            playAudio(sound.bass,volume)
-        break;
-        case 's':
-            playAudio(sound.crash,volume)
-        break;
-        case 'd':
-            playAudio(sound.floorTom,volume)
-            break;
-        case 'f':
-            playAudio(sound.highTom,volume)
-        break;
-        case 'h':
-            playAudio(sound.hihat,volume)
-        break;
-        case 'j':
-            playAudio(sound.lowTom,volume)
-        break;
-        case 'k':
-            sound.ride.currentTime =0.1
-            playAudio(sound.ride,volume)
-        break;
-        case 'l':
-            playAudio(sound.snare,volume)
-    }
-})
+document.addEventListener("keydown", (e) => {
+  const key = e.key.toLowerCase();
+  e.stopPropagation();
+  switch (key) {
+    case "a":
+      playAudio(sound.hihat, volume);
+      animateDrumstick(drumstickHihat, "hihat");
+      break;
+
+    case "s":
+      playAudio(sound.crash, volume);
+      animateDrumstick(drumstickCrash, "crash");
+      break;
+    case "d":
+      playAudio(sound.snare, volume);
+      animateDrumstick(drumstickSnare, "snare");
+      break;
+    case "f":
+      playAudio(sound.highTom, volume);
+      animateDrumstick(drumstickHighTom, "high-tom");
+      break;
+    case "h":
+      playAudio(sound.bass, volume);
+      break;
+
+    case "j":
+      playAudio(sound.lowTom, volume);
+      animateDrumstick(drumstickLowTom, "low-tom");
+      break;
+    case "k":
+      sound.ride.currentTime = 0.1;
+      playAudio(sound.ride, volume);
+      animateDrumstick(drumstickRide, "ride");
+      break;
+    case "l":
+      playAudio(sound.floorTom, volume);
+      animateDrumstick(drumstickFloorTom, "floor-tom");
+      break;
+  }
+});
 //*Change the volume
-volumeInput.addEventListener('change',(e)=>{
-    volume = e.target.value /10
-    volumeIndicator.innerText = `${e.target.value*10}%`
-})
+volumeInput.addEventListener("change", (e) => {
+  volume = e.target.value / 10;
+  volumeIndicator.innerText = `${e.target.value * 10}%`;
+});
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,27 @@
+// const leftDrumstick = document.querySelector('.drumstick-default-left')
+// const rightDrumstick = document.querySelector('.drumstick-default-right')
+const drumsticks = document.querySelectorAll('.drumsticks')
+
 export function playAudio(audio,volume){
     audio.currentTime = 0
     audio.volume = volume
     audio.play()
+}
+export function animateDrumstick(element,partName){
+    element.classList.remove(`${partName}-drumstick-animation`)
+    element.classList.add(`${partName}-drumstick-animation`)
+    setOpacity(element)
+    setTimeout(()=>{
+        element.classList.remove(`${partName}-drumstick-animation`);
+    },100)
+}
+function setOpacity(element){
+    drumsticks.forEach(drumstick=>{
+        console.log("drumstick",drumstick)
+        console.log(element)
+        console.log(drumstick == element);
+        drumstick == element
+          ? (drumstick.style.opacity = 1)
+          : (drumstick.style.opacity = 0);
+    })
 }
